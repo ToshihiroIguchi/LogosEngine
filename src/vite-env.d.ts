3bef7295de77e5974e275a5932f2fdd332ab50b7
Toshihiro Iguchi <toshihiro.iguchi.mail@gmail.com>
2026-01-14 23:26:30 +0900

    Fix KaTeX build error by adding type declarations

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,3 +2,22 @@
 
 declare const __APP_VERSION__: string
 declare const __COMMIT_HASH__: string
+
+declare module 'katex/dist/contrib/auto-render' {
+    import { KatexOptions } from 'katex';
+    
+    interface AutoRenderOptions extends KatexOptions {
+        delimiters?: Array<{
+            left: string;
+            right: string;
+            display: boolean;
+        }>;
+        ignoredTags?: string[];
+        ignoredClasses?: string[];
+        errorCallback?: (msg: string, err: Error) => void;
+        preProcess?: (math: string) => string;
+    }
+
+    function renderMathInElement(element: HTMLElement, options?: AutoRenderOptions): void;
+    export default renderMathInElement;
+}
